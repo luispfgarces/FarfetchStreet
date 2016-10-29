0b1117c93c95a302c991a27ca385263c5bb7c680
Ricardo Vilela <ricardovilela@rvilela.local>
2016-10-30 00:31:09 +0100

    Walk Player

---
+++ Street_Farfetch.com/Assets/CameraFollow.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraFollow : MonoBehaviour {
+
+
+	public Transform car;
+
+	public Vector3 wantedPos;
+	public float distance = 6.4f;
+	public float height = 1.4f;
+	public float rotationDamping = 3.0f;
+	public float heightDamping = 2.0f;
+	public float zoonRacio = 0.5f;
+	private Vector3 roationVector;
+
+	// Use this for initialization
+	void Start () {
+
+	}
+
+	// Update is called once per frame
+	void LateUpdate () {
+
+		float wantedAngel = car.eulerAngles.y;
+		float wantedHeight = car.position.y + height;
+		float myAngel = transform.eulerAngles.y;
+		float myHeight = transform.position.y;
+
+		myAngel = Mathf.LerpAngle (myAngel, wantedAngel, rotationDamping * Time.deltaTime);
+		myHeight = Mathf.Lerp (myHeight, wantedHeight, heightDamping * Time.deltaTime);
+
+		var currentRotation = Quaternion.Euler (0f,myAngel, 0f);
+		transform.position = car.position;
+		transform.position -= currentRotation * Vector3.forward * distance;
+
+		transform.position = new Vector3 (transform.position.x, myHeight, transform.position.z);
+
+		transform.LookAt (car);
+
+	}
+}
--- Street_Farfetch.com/Assets/PlayerMovement.cs
+++ Street_Farfetch.com/Assets/PlayerMovement.cs
@@ -3,16 +3,21 @@ using System.Collections;
 
 public class PlayerMovement : MonoBehaviour {
 
-	public float speed = 6f;
-	Vector3 movement;
-	Animator anim;
-	Rigidbody playerRigidBody;
+	public float speed = 1f;
+	Vector3 movement;                   // The vector to store the direction of the player's movement.
+	Animator anim;                      // Reference to the animator component.
+	Rigidbody playerRigidbody;          // Reference to the player's rigidbody.
+	int floorMask;                      // A layer mask so that a ray can be cast just at gameobjects on the floor layer.
+	float camRayLength = 10f; 
 
 	void Awake(){
 		
-		anim = GetComponent<Animator> ();
+		// Create a layer mask for the floor layer.
+		floorMask = LayerMask.GetMask ("Floor");
 
-		playerRigidBody = GetComponent<Rigidbody> ();
+		// Set up references.
+		anim = GetComponent <Animator> ();
+		playerRigidbody = GetComponent <Rigidbody> ();
 
 	}
 
@@ -22,16 +27,42 @@ public class PlayerMovement : MonoBehaviour {
 		float v = Input.GetAxisRaw ("Vertical");
 
 		Move (h, v);
+		//Turning ();
 		Animating (h, v);
 	}
 
 	void Move(float h, float v){
 
 		movement.Set (h, 0, v);
-		movement = movement.normalized * speed * Time.deltaTime;
-		playerRigidBody.MovePosition (transform.position + movement);
-	}
 
+		movement = movement.normalized * speed * Time.deltaTime;
+
+		playerRigidbody.MovePosition (transform.position + movement);
+	}
+	void Turning ()
+	{
+		// Create a ray from the mouse cursor on screen in the direction of the camera.
+		Ray camRay = Camera.main.ScreenPointToRay (Input.mousePosition);
+
+		// Create a RaycastHit variable to store information about what was hit by the ray.
+		RaycastHit floorHit;
+
+		// Perform the raycast and if it hits something on the floor layer...
+		if(Physics.Raycast (camRay, out floorHit, camRayLength, floorMask))
+		{
+			// Create a vector from the player to the point on the floor the raycast from the mouse hit.
+			Vector3 playerToMouse = floorHit.point - transform.position;
+
+			// Ensure the vector is entirely along the floor plane.
+			playerToMouse.y = 0f;
+
+			// Create a quaternion (rotation) based on looking down the vector from the player to the mouse.
+			Quaternion newRotation = Quaternion.LookRotation (playerToMouse);
+
+			// Set the player's rotation to this new rotation.
+			playerRigidbody.MoveRotation (newRotation);
+		}
+	}
 	void Animating(float h, float v){
 
 		bool walking = h !=0f || v!=0f;
